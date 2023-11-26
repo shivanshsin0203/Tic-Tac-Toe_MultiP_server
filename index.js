c066ea28e837a7173791dfa141eabd4522ea0dd0
Shivansh Singh <singhshivansh12may@gmail.com>
2023-11-26 11:15:53 +0530

    Sovled 2 player problem

--- index.js
+++ index.js
@@ -10,21 +10,33 @@ const io = new Server(httpServer, {
     origin: "http://localhost:3000",
     allowedHeaders: ["my-custom-header"],
     credentials: true
-    }
+  }
 });
+
 io.on('connection', (socket) => {
-    console.log('a user connected ' + socket.id);
-    socket.on('join_room',(data)=>{
-      console.log("joining room "+data.roomid)
-      socket.join(data.roomid);
-      const room = io.sockets.adapter.rooms.get(data.roomid);
-      const numPlayers = room ? room.size : 0;
-  
-      io.to(data.roomid).emit('updatePlayers', numPlayers);
-  
-      console.log(`Player joined room ${data.roomid}. Number of players: ${numPlayers}`);
-  })
+  console.log('a user connected ' + socket.id);
+
+  socket.on('join_room', (data) => {
+    console.log("joining room " + data.roomid)
+    socket.join(data.roomid);
+    const room = io.sockets.adapter.rooms.get(data.roomid);
+    const numPlayers = room ? room.size : 0;
+
+ 
+    const currentPlayer = numPlayers === 1 ? 'X' : 'O';
+
+    
+    io.to(data.roomid).emit('updatePlayers', { numPlayers:numPlayers, currentPlayer:currentPlayer });
+
+    console.log(`Player joined room ${data.roomid}. Number of players: ${numPlayers}`);
+  });
+
+  socket.on('data_send', (data) => {
+    console.log(data);
+    io.to(data.roomid).emit('data_receive', data);
+  });
 });
+
 const PORT = process.env.PORT || 3001;
 
 httpServer.listen(PORT, () => {
